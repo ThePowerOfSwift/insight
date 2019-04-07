a8f9c9dd80b84d95c403f9722441dd2b04f9a503
dmandarino <Douglas Mandarino>
2019-04-07 01:56:42 -0300

    adding replaykit to record and timer

--- Insight/Insight/Modules/Insight/View/InsightViewController.swift
+++ Insight/Insight/Modules/Insight/View/InsightViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import UIKit
+import ReplayKit
 
 
 class InsightViewController: UIViewController {
     
+    @IBOutlet weak var recordBarView: RecordBarView!
+    
     private var presenter: InsightPresenterProtocol?
     
-    @IBOutlet weak var recordBarView: RecordBarView!
+    private let controller = RPBroadcastController()
+    private let recorder = RPScreenRecorder.shared()
+    
+    private var toogleTest: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureInsightViewController()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("Memory warning triggered. If running this app on an older device, you might want to remove some apps from multitasking.")
     }
     
     func setupViewController(with presenter: InsightPresenterProtocol) {
@@ -41,5 +52,63 @@ extension InsightViewController: RecordBarDelegate {
     
     func didTapRecordButton() {
         print("RECORD")
+        if toogleTest {
+            recordBarView.didStartRecording()
+            toogleTest = false
+        } else {
+            recordBarView.didStopRecording()
+            toogleTest = true
+        }
+//        controller.isBroadcasting ? stopBroadcast() : startBroadcast()
+    }
+}
+
+extension InsightViewController: RPBroadcastActivityViewControllerDelegate {
+    
+    func broadcastActivityViewController(_ broadcastActivityViewController: RPBroadcastActivityViewController, didFinishWith broadcastController: RPBroadcastController?, error: Error?) {
+        
+        guard error == nil else {
+            print("Broadcast Activity Controller is not available.")
+            return
+        }
+        
+        broadcastActivityViewController.dismiss(animated: true) {
+            broadcastController?.startBroadcast { error in
+                if error == nil {
+                    print("Broadcast started successfully!")
+                    self.broadcastStarted()
+                }
+            }
+        }
+    }
+    
+    private func startBroadcast() {
+        RPBroadcastActivityViewController.load { broadcastAVC, error in
+            guard error == nil else {
+                print("Cannot load Broadcast Activity View Controller.")
+                return
+            }
+            if let broadcastAVC = broadcastAVC {
+                broadcastAVC.delegate = self
+                self.present(broadcastAVC, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    private func stopBroadcast() {
+        controller.finishBroadcast { error in
+            if error == nil {
+                print("Broadcast ended")
+                self.broadcastEnded()
+            }
+        }
+    }
+    
+    private func broadcastStarted() {
+        recordBarView.didStartRecording()
+    }
+    
+    private func broadcastEnded() {
+        recordBarView.didStartRecording()
     }
 }
